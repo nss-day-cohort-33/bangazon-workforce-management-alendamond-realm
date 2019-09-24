9c44ba2d074d9abf0036b86923f434be9d4e2e53
Mary West <westmarye1219@gmail.com>
2019-09-24 15:31:22 -0500

    added data showing on the page

--- hrapp/views/employees/employee_list.py
+++ hrapp/views/employees/employee_list.py
@@ -1,5 +1,7 @@
 import sqlite3
 from django.shortcuts import render
+from django.urls import reverse
+from django.shortcuts import redirect
 from hrapp.models import Employee
 from ..connection import Connection
 from hrapp.models import model_factory
@@ -22,22 +24,11 @@ def employee_list(request):
                 e.start_date,
                 e.is_supervisor
             from hrapp_employee e
-            join auth_user u on l.user_id = u.id
+            join auth_user u on e.user_id = u.id
             """)
 
-            all_employees = []
-            dataset = db_cursor.fetchall()
+            all_employees = db_cursor.fetchall()
 
-            for row in dataset:
-                employee = Employee()
-                employee.id = row['id']
-                employee.first_name = row['first_name']
-                employee.last_name = row['last_name']
-                employee.start_date = row['start_date']
-                employee.is_supervisor = row['is_supervisor']
-                # employee.department = row['department']
-
-                all_employees.append(employee)
 
     template = 'employees/employees_list.html'
     context = {
@@ -45,3 +36,7 @@ def employee_list(request):
     }
 
     return render(request, template, context)
+
+    return redirect(reverse('hrapp:employees'))
+
+
