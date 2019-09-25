1ba7a8ca5b70a27beba8ba58cc07a9a99eb02c48
Curt Cato <ccato829@gmail.com>
2019-09-25 11:16:13 -0500

    added department fixture

--- hrapp/urls.py
+++ hrapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.conf.urls import include
 from hrapp import views
 from .views import *
+from django.conf.urls import url
 
 app_name = 'hrapp'
 
@@ -13,4 +14,8 @@ urlpatterns = [
     path('departments/', department_list, name='department_list'),
     path('departments/<int:department_id>/', department_details, name='department_details'),
     path('^department/form$', department_form, name='department_form'),
+    path('computer/', computer_list, name='computer_list'),
+    path('computer/form', computer_form, name='computer_form')
 ]
+
+
--- hrapp/views/__init__.py
+++ hrapp/views/__init__.py
@@ -1,6 +1,11 @@
 from .employees.employee_list import employee_list
 from .home import home
 from .auth.logout import logout_user
+<<<<<<< HEAD
 from .departments.department_list import department_list
 from .departments.department_details import department_details
 from .departments.department_form import department_form
+=======
+from .computers import computer_list
+from .computers import computer_form
+>>>>>>> master
